2c5325138f02056fd0c53ceec1f5a90dcc51e5a0
Marcus Chang <30785471+Rynhalt@users.noreply.github.com>
2025-06-04 17:53:02 +0900

    fixed next config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['img.theapi.app', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-   module.exports = {
-     eslint: {
-       ignoreDuringBuilds: true,
-     },
-   };
-
-export default nextConfig;
+module.exports = nextConfig;
